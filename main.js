e6e49d44f6312b0e2aea7541b1d66756d7b59ae0
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-09-12 19:48:26 -0300

    Update main,js

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 function calcular() {
 
-    var peso = window.document.getElementById('txt1');
-    var altura = window.document.getElementById('txt2');
-    var res = window.document.querySelector('div#res')
-    var pesoCalcular = Number(peso.value);
-    var alturaCalcular = Number(altura.value);
-    var imcCalcular = pesoCalcular / (alturaCalcular * alturaCalcular);
-    if (peso.value.length == 0 || peso.value > 300 ) {
-        alert('[ERRO] Verifique os dados e tente novamente')
-    } 
+        var peso = window.document.getElementById('txt1');
+        var altura = window.document.getElementById('txt2');
+        var res = window.document.querySelector('div#res')
+        var pesoCalcular = Number(peso.value);
+        var alturaCalcular = Number(altura.value);
+        var imcCalcular = pesoCalcular / (alturaCalcular * alturaCalcular);
     
-    /*if (altura.value.length == 0 || altura.value > 2.00 ) {
-        alert('[ERRO] Verifique os dados e tente novamente')
-    }
-    else {
-        (altura.value.length == 0 || altura.value > 2.00 )
-        alert('[ERRO] Verifique os dados e tente novamente')
+        if (peso.value == 0 || peso.value > 300 ) {
+            alert('[ERRO] Verifique os dados e tente novamente')
+        }
+        if (altura.value == 0 || altura.value > 2.00 ) {
+            alert('[ERRO] Verifique os dados e tente novamente 6')
 
-    } */
+        } 
+
+        res.innerHTML = `Seu IMC atual é ${Math.round(imcCalcular)}`
+
+    }
+
  
-
-    res.innerHTML = `Seu IMC atual é ${Math.round(imcCalcular)}`
-
-
-    }
-
-
     //<input type="button" id="button" value="Calcular" onclick="calcular()">
 
    
